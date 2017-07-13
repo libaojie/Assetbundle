32917851f8ea3157ac0e1c8516785d92abc35e0b
libaojie <leebaojie@sina.com>
2017-07-13 15:46:23 +0800

    下载正常

---
+++ Assetbundle/Assets/Script/Controller/Download.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Download : MonoBehaviour {
+
+	public string baseURL = "ftp://192.168.0.173/CXL";
+	public string name = "AssetBundles_v1.0.zip";
+
+	DownloadManager downloadManager; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		downloadManager = gameObject.AddComponent<DownloadManager>();
+		downloadManager.Init();
+	}
+	
+
+
+	void OnGUI()
+	{
+		if (GUILayout.Button("Download Assetbundle"))
+		{
+			StartCoroutine(downloadManager.DownLoadAssetBundle(name, baseURL));
+		}
+	}
+}
---
+++ Assetbundle/Assets/Script/Manager/DownloadManager.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+/// <summary>
+/// 下载管理器
+/// </summary>
+public class DownloadManager : MonoBehaviour 
+{
+	private string tempPath = Application.streamingAssetsPath + "/Temp";
+
+
+	private static DownloadManager instance = null;
+	public static DownloadManager Instance
+	{
+		get
+		{
+			return instance;
+		}
+	}
+
+	/// <summary>
+	/// 初始化函数
+	/// </summary>
+	public void Init()
+	{
+		instance = this;
+	}
+
+	/// <summary>
+	/// 释放函数
+	/// </summary>
+	public void Release()
+	{
+		instance = null;
+	}
+
+	/// <summary>
+	/// 下载AssetBundle
+	/// </summary>
+	/// <param name="name"></param>
+	/// <param name="baseUrl"></param>
+	/// <returns></returns>
+	public IEnumerator DownLoadAssetBundle(string name, string baseUrl)
+	{
+		string realName = baseUrl + "/"+ name;
+
+		using (WWW www = new WWW(realName))
+		{
+			www.threadPriority = ThreadPriority.High;
+			while(!www.isDone)
+			{
+				yield return www.progress;
+				ShowLog("Download... "+ www.progress);
+			}
+
+			if (www.error == null)
+			{
+				// 下载成功
+				ShowLog("Download Successfully ");
+				CreateBundleFile(name, www.bytes);
+			}
+			else
+			{
+				ShowLog("Download Fail " + www.error);
+			}
+		}
+	}
+
+
+	private void CreateBundleFile(string name, byte[] info)
+	{
+		/*
+		Stream sw;
+		string[] dir = name.Split('/');
+		string dirpath = tempPath;
+		if (dir.Length > 1)
+		{
+			for (int i = 0; i < dir.Length - 1; i++)
+			{
+				dirpath += "/" + dir[i];
+				if (!Directory.Exists(dirpath))
+				{
+					Directory.CreateDirectory(dirpath);
+				}
+			}
+		}
+
+		FileInfo file = new FileInfo(tempPath + "//" + name);
+		if (!file.Exists)
+		{
+			sw = file.
+		}
+		else
+		{
+			sw = file.OpenWrite();
+		}
+		try
+		{
+			sw.Write(info, 0, info.Length);
+			sw.Close();
+			sw.Dispose();
+		}
+		catch (Exception e)
+		{
+		}*/
+
+		string fileName = tempPath + "/" + name;
+		string[] dir = fileName.Split('/');
+		string dirpath = "";
+		if (dir.Length > 1)
+		{
+			for (int i = 0; i < dir.Length - 1; i++)
+			{
+				if (i == 0)
+				{
+					dirpath = dir[i];
+				}
+				else
+				{
+					dirpath += "/" + dir[i];
+				}
+				
+				if (!Directory.Exists(dirpath))
+				{
+					Directory.CreateDirectory(dirpath);
+				}
+			}
+		}
+
+		FileStream pFileStream = null;
+		try
+		{
+			pFileStream = new FileStream(fileName, FileMode.OpenOrCreate);
+			pFileStream.Write(info, 0, info.Length);
+		}
+		catch
+		{
+		}
+		finally
+		{
+			if (pFileStream != null)
+				pFileStream.Close();
+		}
+		
+	}
+
+	private void ShowLog(string log)
+	{
+		Debug.Log(Time.time+"	"+ log);
+	}
+}
